41df0eeeedfc84e86ecae00bf843c548c17aa9f0
gasperzinha7@gmail.com <gasperzinha7@gmail.com>
2024-09-15 20:09:09 -0300

    correção de arquivos de conexão

--- MotoApi/Models/Moto.cs
+++ MotoApi/Models/Moto.cs
@@ -1,12 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MotoApi.Models
 {
     public class Moto
     {
         public Guid Id { get; set; } // Usar Guid para ID
+        [Required]
         public int Ano { get; set; }
-        public string? Modelo { get; set; }
-        public string? Placa { get; set; }
-
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
         // Você pode adicionar um campo para verificar locações no futuro:
         public bool TemLocacao { get; set; } // campo que indica se a moto possui registro de locação
     }
